1b23b440fd5aa2a93bb075f1aad58ec9a635867d
Francis Hanna <91334875+frankhanna94@users.noreply.github.com>
2025-07-08 12:07:44 -0400

    Update download_edx.R with new Resource IDs

--- download_edx.R
+++ download_edx.R
@@ -27,13 +27,13 @@ output_dir <- file.path(dir_1, target_subfolder)
 
 # Add resource id for the files to be downloaded from EDX
 resource_ids <- list(
-  model_rds = "5da4b595-7261-4e1f-a16e-eb4c7c243bd0",
-  ipcc_gwp_factors = "36699d2f-a1ca-4788-90eb-37f88ebf3a92",
-  examples_grouped = "c9463532-635d-44fa-81f8-744f47acfc2b",
-  eeio_template = "79d9f605-c753-4c03-bcd4-46b569dbcad3",
-  inventory_cw = "f9c8ff07-7b7d-4b5b-8728-6d0ba809a404",
-  example_upload = "2397cfda-e47a-4eb3-beb0-f9ba64be6052",
-  dict_sheet = "08b2e3a8-3de1-41bb-9d1c-0f14d1f8d299"
+  model_rds = "d7da5bd4-7b3d-443b-8f5a-38f0212577be",
+  ipcc_gwp_factors = "416b3a71-94df-4a98-8b3e-a96cf0195574",
+  examples_grouped = "90958b5b-4a1d-4ae0-9c85-834dfcc72997",
+  eeio_template = "dd3e6f8a-c77f-4443-b94b-c8f6a03af29f",
+  inventory_cw = "b810ebcd-4e5e-4298-9158-d5598089e0ab",
+  example_upload = "1605ecfc-2b02-46b5-831d-9ae1837f930c",
+  dict_sheet = "6ff80ddd-0dbb-4ba0-90ec-c4e1f0a224c0"
 )
 edx_url <- "https://edx.netl.doe.gov/resource/"
 edx_dl <- "/download"
